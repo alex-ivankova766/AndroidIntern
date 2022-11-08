e6ff76904b2eb7384a1647fa4648c6c47d30aaac
immersion <radadabro@gmail.com>
2022-11-08 19:39:55 +0500

    Task 5 в ходе выполнения, нужен рестарт иде

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.example.androidintern
 
 import android.os.Bundle
-import android.view.Gravity
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText = findViewById<EditText>(R.id.editText)
         val button = findViewById<Button>(R.id.button_ok)
         button.setOnClickListener {
-            val duration = Toast.LENGTH_SHORT
-            val toast = Toast.makeText(applicationContext, R.string.ok_clicked, duration)
-            toast.setGravity(Gravity.CENTER, 0, 0)
-            toast.show()
+            var someText = editText.getText().toString()
+            println(someText)
         }
+
     }
 }
