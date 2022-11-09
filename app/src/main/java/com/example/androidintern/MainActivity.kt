c60e2edd2727feb6734b1d02909aba47954b3706
immersion <radadabro@gmail.com>
2022-11-09 13:05:18 +0500

    Task 5 выполнено

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.androidintern
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
-import timber.log.Timber.DebugTree
+
 
 private const val LOG_TAG = "MY LOG"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.editText)
         val button = findViewById<Button>(R.id.button_ok)
         button.setOnClickListener {
-            var someText = editText.getText().toString()
-            Log.i(LOG_TAG, "someText: $someText")
+            val someText = editText.text.toString()
+            Timber.tag(LOG_TAG).d("someText: $someText")
         }
     }
 }
