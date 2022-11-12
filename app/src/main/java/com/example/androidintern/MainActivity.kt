146ba44e216e21836879184da1713fe955d90a17
immersion <radadabro@gmail.com>
2022-11-12 11:09:58 +0500

    Task 8 выполнено

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.androidintern
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val view = findViewById<TextView>(R.id.view)
+        button.setOnClickListener {
+            view.setText(R.string.button_pressed)
         }
+    }
 }
