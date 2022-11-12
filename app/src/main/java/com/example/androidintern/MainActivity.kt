0508068ce3159aa49cdb4691102a7fc5d99d80a0
immersion <radadabro@gmail.com>
2022-11-12 17:41:40 +0500

    Task 9 выполнено

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.androidintern
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,9 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button)
-        val view = findViewById<TextView>(R.id.view)
+        val view = findViewById<TextView>(R.id.text_view)
+        val checkBox = findViewById<CheckBox>(R.id.check_box)
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
         button.setOnClickListener {
-            view.setText(R.string.button_pressed)
+            if (checkBox.isChecked && !TextUtils.isEmpty(editText.text.toString())) {
+                view.text = editText.text.toString()
+                progressBar.progress += 10
+            }
         }
     }
 }
