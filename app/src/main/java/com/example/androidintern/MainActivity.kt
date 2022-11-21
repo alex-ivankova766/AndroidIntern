5fd995175edd543478196c81f2dbbe8c79303b53
immersion <radadabro@gmail.com>
2022-11-21 16:44:14 +0500

    Task14 выполнено

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -2,42 +2,16 @@ package com.example.androidintern
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import java.io.IOException
-import java.net.URL
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import timber.log.Timber
-
-private const val LOG_TAG = "MY_LOG"
-private const val TASK_URL =
-    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1"
-private val client = OkHttpClient()
+import com.example.androidintern.databinding.ActivityMainBinding
 
 class MainActivity() : AppCompatActivity() {
+    lateinit var bndActivityMain: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        getData()
-    }
-
-    private fun getData() {
-        val request = Request.Builder()
-            .url(URL(TASK_URL))
-            .build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Timber.tag(LOG_TAG).d(getString(R.string.error))
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (!response.isSuccessful) throw IOException("${R.string.error} $response")
-                    Timber.tag(LOG_TAG).d(response.body!!.string())
-                }
-            }
-        })
+        bndActivityMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bndActivityMain.root)
+        bndActivityMain.bt.setOnClickListener {
+            bndActivityMain.tvResult.text = bndActivityMain.edt.text
+        }
     }
 }
