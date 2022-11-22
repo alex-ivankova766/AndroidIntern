4323fbcdd989c5161c7561d8a2fe3a4558a6fbdc
immersion <radadabro@gmail.com>
2022-11-22 11:26:31 +0500

    Task15 выполнено

--- app/src/main/java/com/example/androidintern/MainActivity.kt
+++ app/src/main/java/com/example/androidintern/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.androidintern
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.androidintern.databinding.ActivityMainBinding
 
+private const val URL = "https://bigpicture.ru/wp-content/uploads/2012/04/2184.jpg"
+
 class MainActivity() : AppCompatActivity() {
-    lateinit var bndActivityMain: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bndActivityMain = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bndActivityMain.root)
-        bndActivityMain.bt.setOnClickListener {
-            bndActivityMain.tvResult.text = bndActivityMain.edt.text
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val imageView = findViewById<ImageView>(R.id.ivPhotoFromUrl)
+        binding.btActivitySwitch.setOnClickListener {
+            setContentView(R.layout.photo_from_url)
+            Glide.with(this)
+                .load(URL)
+                .into(imageView)
         }
     }
 }
