f5175917eaa359bc063209b6bf4cc00409b05479
immersion <radadabro@gmail.com>
2022-12-02 13:29:08 +0500

    Task18 выполнено

--- app/src/main/java/com/example/androidintern/SecondActivity.kt
+++ app/src/main/java/com/example/androidintern/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidintern
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -9,10 +11,26 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
         val ivBigCat = findViewById<ImageView>(R.id.ivBigCat)
         val bigPhotoLink = intent.getStringExtra("bigPhotoLink")
         Glide.with(this)
             .load(bigPhotoLink)
             .into(ivBigCat)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_second, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.itemLike) {
+            super.finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
